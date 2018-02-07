384f55c2eac6bc568d5addae2ab4f16f720a7203
mostafa-tharwat <33877788+mostafa-tharwat@users.noreply.github.com>
2018-02-08 00:35:07 +0200

    first commit

---
+++ password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pass, input;
+            pass = "111";
+            Console.Write("\n  Enter Your Password : ");
+            input = Console.ReadLine();
+
+            while (input != pass)
+            {
+                Console.WriteLine("\a *.* Error Password *.* ");
+
+                Console.Write(" Enter Your Password : ");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("\t WElcome ");
+            Console.ReadKey();
+        }
+    }
+}
